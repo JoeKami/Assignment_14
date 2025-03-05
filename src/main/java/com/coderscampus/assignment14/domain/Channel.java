ca618d5ad8e51a894cee2bb9410359e890797693
JoeKami <j.b.carrasquillo@gmail.com>
2025-03-05 00:08:13 -0500

    Setting up structure with repo and service

---
+++ src/main/java/com/coderscampus/assignment14/domain/Channel.java
@@ -0,0 +1,7 @@
+package com.coderscampus.assignment14.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Channel {
+}
---
+++ src/main/java/com/coderscampus/assignment14/domain/User.java
@@ -0,0 +1,34 @@
+package com.coderscampus.assignment14.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    public Long userId;
+    public String username;
+    public String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
